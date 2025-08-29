35d03d44621a19d3cdf659af18cf10ee8c31bbe7
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-08-28 21:50:09 -0300

    Implementada a adição de Medicamentos Prescritos

--- ControleDeMedicamentos/Controllers/PrescricaoController.cs
+++ ControleDeMedicamentos/Controllers/PrescricaoController.cs
@@ -7,7 +7,7 @@ using ControleDeMedicamentos.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace ControleDeMedicamentos.Controllers;
+namespace ControleDeMedicamentos.WebApp.Controllers;
 
 public class PrescricaoController : Controller
 {
@@ -75,5 +75,56 @@ public class PrescricaoController : Controller
         repositorioPrescricao.CadastrarRegistro(entidade);
 
         return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult Gerenciar(Guid id)
+    {
+        var medicamentosDisponiveis = repositorioMedicamento.SelecionarRegistros();
+
+        var prescricaoSelecionada = repositorioPrescricao.SelecionarRegistroPorId(id);
+
+        var gerenciarVm = new GerenciarPrescricaoViewModel(
+            id,
+            prescricaoSelecionada.Descricao,
+            prescricaoSelecionada.CrmMedico,
+            prescricaoSelecionada.Paciente.Id,
+            prescricaoSelecionada.Paciente.Nome,
+            prescricaoSelecionada.MedicamentosPrescritos,
+            medicamentosDisponiveis
+        );
+
+        return View(gerenciarVm);
+    }
+
+    [HttpPost]
+    public IActionResult AdicionarMedicamentoPrescrito(Guid idPrescricao, AdicionarMedicamentoPrescritoViewModel adicionarMedicamentoVm)
+    {
+        var prescricaoSelecionada = repositorioPrescricao.SelecionarRegistroPorId(idPrescricao);
+
+        var medicamentoSelecionado = repositorioMedicamento.SelecionarRegistroPorId(adicionarMedicamentoVm.MedicamentoId);
+
+        prescricaoSelecionada.AdicionarMedicamentoPrescrito(
+            medicamentoSelecionado,
+            adicionarMedicamentoVm.DosagemMedicamento,
+            adicionarMedicamentoVm.PeriodoMedicamento,
+            adicionarMedicamentoVm.QuantidadeMedicamento
+        );
+
+        contexto.Salvar();
+
+        return RedirectToAction(nameof(Gerenciar), new { id = idPrescricao });
+    }
+
+    [HttpPost]
+    public IActionResult RemoverMedicamentoPrescrito(Guid idPrescricao, Guid idMedicamentoPrescrito)
+    {
+        var prescricaoSelecionada = repositorioPrescricao.SelecionarRegistroPorId(idPrescricao);
+
+        prescricaoSelecionada.RemoverMedicamentoPrescrito(idMedicamentoPrescrito);
+
+        contexto.Salvar();
+
+        return RedirectToAction(nameof(Gerenciar), new { id = idPrescricao });
     }
 }
